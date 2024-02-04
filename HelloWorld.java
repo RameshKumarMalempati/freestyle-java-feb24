45533e11670074bc8d784cf6d3ad718f86081952
Ramesh Kumar M <rk.malempati@gmail.com>
2024-02-04 15:04:06 +0000

    One more change to test Jenkins POLL SCM

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World...made minor change for testing POLL SCM in Jenkins");
+		System.out.println("One more Jenkins relevance change");
+		
 	}
 }
